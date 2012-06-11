d654d0e9ec8855691a8ff65116d399b10025b80c
Noel Rappin <noel@groupon.com>
2012-06-11 14:41:45 -0500

    passing tests

---
+++ spec/active_record_spec_helper.rb
@@ -0,0 +1,29 @@
+$: << File.expand_path("app")
+require 'active_record'
+require 'database_cleaner'
+require 'pry'
+require 'awesome_print'
+
+connection_info = YAML.load(File.open("config/database.yml"))["test"]
+ActiveRecord::Base.establish_connection(connection_info)
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+def require_number_modules
+  require 'active_support/core_ext/date/conversions'
+  require 'active_support/core_ext/hash/keys'
+  require 'active_support/core_ext/hash/reverse_merge'
+  require 'active_support/i18n'
+  require 'action_view/helpers/number_helper'
+end
--- spec/presenters/trip_presenter_spec.rb
+++ spec/presenters/trip_presenter_spec.rb
@@ -1,23 +1,21 @@
-require 'fast_spec_helper'
+require 'active_record_spec_helper'
 require_number_modules
-require 'scot_travel/presenters/trip_presenter' 
 require 'scot_travel/services/date_range_service'
+require 'models/trip'
+require 'scot_travel/presenters/trip_presenter' 
+
 
 module Presenters
-  
-  class DumbTrip < OpenStruct
-    include Services::DateRangeService
-  end
 
   describe TripPresenter do
     it "presents trips" do
-      TripPresenter.present_trips([DumbTrip, DumbTrip]).map(&:class)
+      TripPresenter.present_trips([Trip, Trip]).map(&:class)
           .should == [TripPresenter, TripPresenter]
     end
 
     describe "with one presenter" do
       it "prints a date span" do 
-        trip = DumbTrip.new(
+        trip = Trip.new(
             :start_date => Date.parse("January 22, 1971"), 
             :end_date => Date.parse("May 31, 1971"))
         presenter = TripPresenter.new(trip)
@@ -25,7 +23,7 @@ module Presenters
       end
 
       it "prints a blank date span" do
-        trip = DumbTrip.new(:start_date => nil, :end_date => nil)
+        trip = Trip.new(:start_date => nil, :end_date => nil)
         presenter = TripPresenter.new(trip)
         presenter.date_span.should be_blank
       end
